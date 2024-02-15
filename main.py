0b34cb721301b19d8e6d360aab69d70307699315
tylermarshall24 <160160017+tylermarshall24@users.noreply.github.com>
2024-02-15 09:02:04 -0800

    Hello upload

---
+++ main.py
@@ -0,0 +1,109 @@
+# This file was created by: Tyler Marshall
+
+# importing pygame
+import pygame as pg
+from settings import *
+from sprites import *
+from random import randint
+import sys
+from os import path
+
+
+# Define game class...
+class Game:
+    # Define a special method to init the properties of said class...
+    def __init__(self):
+        # init pygame
+        pg.init()
+        # set size of screen and be the screen
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        # setting game clock 
+        self.clock = pg.time.Clock()
+        self.load_data()
+    def load_data(self):
+        game_folder = path.dirname(__file__)
+        self.map_data = []
+        # 'r'     open for reading (default)
+        # 'w'     open for writing, truncating the file first
+        # 'x'     open for exclusive creation, failing if the file already exists
+        # 'a'     open for writing, appending to the end of the file if it exists
+        # 'b'     binary mode
+        # 't'     text mode (default)
+        # '+'     open a disk file for updating (reading and writing)
+        # 'U'     universal newlines mode (deprecated)
+        # below opens file for reading in text mode
+        # with 
+        '''
+        The with statement is a context manager in Python. 
+        It is used to ensure that a resource is properly closed or released 
+        after it is used. This can help to prevent errors and leaks.
+        '''
+        with open(path.join(game_folder, 'map.txt'), 'rt') as f:
+            for line in f:
+                print(line)
+                self.map_data.append(line)
+
+
+    # Create run method which runs the whole GAME
+    def new(self):
+        self.all_sprites = pg.sprite.Group()
+        self.walls = pg.sprite.Group()
+        self.player1 = Player(self, 1, 1)
+        self.all_sprites.add(self.player1)
+        for x in range(10, 20):
+            Wall(self, x, 5)
+        
+
+    def run(self):
+        # 
+        self.playing = True
+        while self.playing:
+            self.dt = self.clock.tick(FPS) / 1000
+            self.events()
+            self.update()
+            self.draw()
+    def quit(self):
+         pg.quit()
+         sys.exit()
+
+    def update(self):
+        self.all_sprites.update()
+    
+    def draw_grid(self):
+         for x in range(0, WIDTH, TILESIZE):
+              pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+         for y in range(0, HEIGHT, TILESIZE):
+              pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
+
+    def draw(self):
+            self.screen.fill(BGCOLOR)
+            self.draw_grid()
+            self.all_sprites.draw(self.screen)
+            pg.display.flip()
+
+    def events(self):
+         for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.quit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_LEFT:
+                    self.player1.move(dx=-1)
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_RIGHT:
+                    self.player1.move(dx=1)
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_UP:
+                    self.player1.move(dy=-1)
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_DOWN:
+                    self.player1.move(dy=1)
+
+# Instantiate the game... 
+g = Game()
+# use game method run to run
+#g.show_start_screen()
+while True:
+    g.new()
+    g.run()
+    #g.show_go_screen()
---
+++ settings.py
@@ -0,0 +1,14 @@
+WIDTH = 800
+HEIGHT = 600
+
+FPS = 30
+
+TITLE = "My Fun Game"
+
+TILESIZE = 32
+
+
+BGCOLOR = (0,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+LIGHTGREY = (75,75,75)
